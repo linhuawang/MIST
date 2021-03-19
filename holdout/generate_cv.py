f41a1debe875e37a87c25825ca912301fb82104d
linhuawang <linhua.wang1994@gmail.com>
2021-03-19 12:10:10 -0500

    change ho

--- holdout/generate_cv.py
+++ holdout/generate_cv.py
@@ -11,27 +11,35 @@ from os import mkdir
 def main(data_folder, filt=0.5, norm="cpm", kFold=5):
 	norm_fn = join(data_folder, "norm.csv")
 	raw_fn = join(data_folder, "raw.csv")
+
 	raw, meta = utils.read_ST_data(raw_fn)
+	### Quality control of genes and spots
+	good_genes = raw.columns[((raw>2).sum(axis=0) >= 2)]
+	good_spots = raw.index[((raw>2).sum(axis=1) > 50)]
+	raw = raw.loc[good_spots, good_genes]
+	meta = meta.loc[good_spots,:]
 
 	normed, libsize =  utils.data_norm(raw, norm)
-
 	if norm in ["median", "logMed", "logCPM"]:
 		normed = normed.round(2)
 
+	### Write ground truth
 	normed.to_csv(join(data_folder,"norm.csv"))
 	libsize.to_csv(join(data_folder, "libsize.csv"))
 
-	normFilt = utils.filterGene_sparsity(normed,filt)
-	genes = normFilt.columns.tolist()
-	print("Holdouut %s genes." %(normFilt.shape[1]))
+	### Another quality control
+	#normFilt = utils.filterGene_sparsity(normed,filt)
+	#genes = normFilt.columns.tolist()
+	print("Holdouut %s genes." %(normed.shape[1]))
 	# generate 5 fold cross validation datasets
-	ho_dsets, ho_masks = utils.generate_cv_masks(normFilt, genes,5)
+	_, ho_masks = utils.generate_cv_masks(normed, genes,5)
 
 	for fd in range(kFold):
-		ho_data, ho_mask = ho_dsets[fd], ho_masks[fd]
-		ho_raw = utils.data_denorm(ho_data, join(data_folder, "libsize.csv"), norm)
-		ho_data.to_csv(data_folder + "/ho_data_%d.csv" %fd)
-		ho_raw.to_csv(join(data_folder, "ho_raw_%d.csv" %fd))
+		#ho_data, ho_mask = ho_dsets[fd], ho_masks[fd]
+		ho_mask = ho_masks[fd]
+		#ho_raw = utils.data_denorm(ho_data, join(data_folder, "libsize.csv"), norm)
+		#ho_data.to_csv(data_folder + "/ho_data_%d.csv" %fd)
+		#ho_raw.to_csv(join(data_folder, "ho_raw_%d.csv" %fd))
 		ho_mask.to_csv(data_folder + "/ho_mask_%d.csv" %fd)
 		print("[Fold %d] Hold out data generated, %d genes, %d spots." \
 			%(fd, ho_data.shape[1], ho_data.shape[0]))
--- holdout/holdout_experiments.py
+++ holdout/holdout_experiments.py
@@ -9,7 +9,7 @@ import utils
 
 if __name__ == '__main__':
 	folder = sys.argv[1]
-	norm = "logMed"
+	#norm = "logMed"
 	ep = 0.7
 	merge = 0
 	radius = 1.9
@@ -18,26 +18,29 @@ if __name__ == '__main__':
 		merge = 50
 		radius = 100
 
+	count, meta = utils.read_ST_data(os.path.join(folder, "norm.csv"))
 	for i in range(5):
-		fn = os.path.join(folder, "ho_data_%d.csv" %i)
-		data = Data.Data(countpath=fn, radius=radius, merge=merge)
+		mask = pd.read_csv(os.path.join(folder, "ho_mask_%d.csv" %i), index_col=0)
+		mask = mask.loc[count.index, count.columns]
+		ho  = count.copy()
+		ho[mask == 1] = 0
+		# fn = os.path.join(folder, "ho_data_%d.csv" %i)
+		# data = Data.Data(countpath=fn, radius=radius, merge=merge)
+		data = Data.Data(count=count, meta=meta, radius=radius, merge=merge)
 		
 		for imputer_name in ["MAGIC", "knnSmooth", "mcImpute", "spKNN", "spImpute"]:
 			norm_outF = os.path.join(folder, "%s_%d.csv" %(imputer_name, i))
-			raw_outF = os.path.join(folder, "%s_raw_%d.csv" %(imputer_name, i))
-			libsizeF = os.path.join(folder, "libsize.csv")
-		
+			# raw_outF = os.path.join(folder, "%s_raw_%d.csv" %(imputer_name, i))
+			# libsizeF = os.path.join(folder, "libsize.csv")
 			st_time = time()
 			imputer = Imputer(imputer_name, data)
 			imputed_data = imputer.fit_transform()
-			imputed_raw = utils.data_denorm(imputed_data, libsizeF, norm)
-
+#			imputed_raw = utils.data_denorm(imputed_data, libsizeF, norm)
 			if imputer_name == "spImpute":
 				ep = imputer.data.epsilon
 				norm_outF = os.path.join(folder, "%s_%d_%d.csv" %(imputer_name, ep*100, i))
-			
 			imputed_data.to_csv(norm_outF)
-			imputed_raw.to_csv(raw_outF)
+#			imputed_raw.to_csv(raw_outF)
 			print("[%d, %s] elapsed %.1f seconds.." %(i, imputer_name, time() - st_time))
 
 	# fn = os.path.join(folder, "ho_data_0.csv")

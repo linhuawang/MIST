0e7749197f12d5a8549f3b0a408fd843ea7888f2
linhuawang <linhua.wang1994@gmail.com>
2021-03-20 17:44:01 -0500

    .

--- holdout/generate_cv.py
+++ holdout/generate_cv.py
@@ -8,7 +8,7 @@ from time import time
 from os.path import exists, join
 from os import mkdir
 
-def main(data_folder, filt=0.5, norm="cpm", kFold=5):
+def main(data_folder, norm="logMed", kFold=2):
 	norm_fn = join(data_folder, "norm.csv")
 	raw_fn = join(data_folder, "raw.csv")
 
@@ -32,23 +32,18 @@ def main(data_folder, filt=0.5, norm="cpm", kFold=5):
 	#genes = normFilt.columns.tolist()
 	print("Holdouut %s genes." %(normed.shape[1]))
 	# generate 5 fold cross validation datasets
-	_, ho_masks = utils.generate_cv_masks(normed, normed.columns.tolist(), 5)
+	ho_dsets, ho_masks = utils.generate_cv_masks(normed, normed.columns.tolist(), 2)
 
 	for fd in range(kFold):
 		#ho_data, ho_mask = ho_dsets[fd], ho_masks[fd]
 		ho_mask = ho_masks[fd]
-		#ho_raw = utils.data_denorm(ho_data, join(data_folder, "libsize.csv"), norm)
-		#ho_data.to_csv(data_folder + "/ho_data_%d.csv" %fd)
-		#ho_raw.to_csv(join(data_folder, "ho_raw_%d.csv" %fd))
 		ho_mask.to_csv(data_folder + "/ho_mask_%d.csv" %fd)
-		# print("[Fold %d] Hold out data generated, %d genes, %d spots." \
-		# 	%(fd, ho_data.shape[1], ho_data.shape[0]))
 
 if __name__ == "__main__":
 	folder = sys.argv[1]
 	norm = sys.argv[2]
 	assert norm in ["cpm", "logCPM", "median", "logMed"]
 	if "slideseq" in folder:
-		main(folder, 0.1,norm=norm)
+		main(folder, norm=norm)
 	else:
-		main(folder, 0.5,norm=norm)
+		main(folder, norm=norm)
